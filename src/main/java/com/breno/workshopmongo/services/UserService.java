e18735469690e6623268108d330c7e865cd6e2d2
Breno <brenodf110@gmail.com>
2024-01-05 10:11:10 -0300

    Implementado findById e tratamento de exceção

--- src/main/java/com/breno/workshopmongo/services/UserService.java
+++ src/main/java/com/breno/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.breno.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.breno.workshopmongo.domain.User;
 import com.breno.workshopmongo.repository.UserRepository;
+import com.breno.workshopmongo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,4 +20,8 @@ public class UserService {
 		return userRepository.findAll();
 	}
 	
+	public User findById(String id) {
+		Optional<User> user = userRepository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
 }
