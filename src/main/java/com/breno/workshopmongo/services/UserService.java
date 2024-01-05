871ee5ee5c0f942a861bf0849452d1ebb4f60f0f
Breno <brenodf110@gmail.com>
2024-01-05 13:21:03 -0300

    Insert user

--- src/main/java/com/breno/workshopmongo/services/UserService.java
+++ src/main/java/com/breno/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.breno.workshopmongo.domain.User;
+import com.breno.workshopmongo.dto.UserDTO;
 import com.breno.workshopmongo.repository.UserRepository;
 import com.breno.workshopmongo.services.exceptions.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = userRepository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return userRepository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
