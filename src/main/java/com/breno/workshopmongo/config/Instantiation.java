3570152893d509b020b4fec6e8be5747ccdfc848
Breno <brenodf110@gmail.com>
2024-01-06 16:12:29 -0300

    Simple query

--- src/main/java/com/breno/workshopmongo/config/Instantiation.java
+++ src/main/java/com/breno/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.breno.workshopmongo.domain.Post;
 import com.breno.workshopmongo.domain.User;
 import com.breno.workshopmongo.dto.AuthorDTO;
+import com.breno.workshopmongo.dto.CommentDTO;
 import com.breno.workshopmongo.repository.PostRepository;
 import com.breno.workshopmongo.repository.UserRepository;
 
@@ -38,8 +39,15 @@ public class Instantiation implements CommandLineRunner{
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
-		Post post1 = new Post(null, sdf.parse("21/03/08"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
-		Post post2 = new Post(null, sdf.parse("23/03/08"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Que bom", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
--- src/main/java/com/breno/workshopmongo/resources/PostResource.java
+++ src/main/java/com/breno/workshopmongo/resources/PostResource.java
@@ -1,25 +1,19 @@
 package com.breno.workshopmongo.resources;
 
-import java.net.URI;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.breno.workshopmongo.domain.Post;
-import com.breno.workshopmongo.domain.User;
-import com.breno.workshopmongo.dto.UserDTO;
+import com.breno.workshopmongo.resources.util.URL;
 import com.breno.workshopmongo.services.PostService;
+
 
 @RestController
 @RequestMapping(value="/posts")
@@ -34,4 +28,10 @@ public class PostResource {
 		return ResponseEntity.ok().body(obj);
 	}
 	
+	@GetMapping(value="/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text){
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
 }
