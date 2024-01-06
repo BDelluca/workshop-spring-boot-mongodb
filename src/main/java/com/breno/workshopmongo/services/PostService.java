53015f8c8f1a0dd9006a2103dc19b74f49bf6304
Breno <brenodf110@gmail.com>
2024-01-06 14:06:40 -0300

    Criando Post service e resource

---
+++ src/main/java/com/breno/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+
+package com.breno.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.breno.workshopmongo.domain.Post;
+import com.breno.workshopmongo.repository.PostRepository;
+import com.breno.workshopmongo.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository postRepository;
+	
+	public Post findById(String id) {
+		Optional<Post> post = postRepository.findById(id);
+		return post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+}
